ad13eb0e840645e94cd8b3afb61c1b81d9508d18
Vince Au <vinceau09@gmail.com>
2018-12-17 22:25:17 +1100

    Fix indentation

--- splittext.js
+++ splittext.js
@@ -1,9 +1,9 @@
-function SplitText(identifier, vars){
+function SplitText(identifier, vars) {
 
-	function duplicateObject(obj){
-		if(typeof obj == "object" && obj !== null){
+	function duplicateObject(obj) {
+		if (typeof obj == "object" && obj !== null) {
 			var ret = {};
-			for(var index in obj){
+			for (var index in obj) {
 				ret[index] = duplicateObject(obj[index]);
 			}
 			return ret;
@@ -12,57 +12,57 @@ function SplitText(identifier, vars){
 		}
 	}
 
-	String.prototype.replaceAll = function(search, replacement) {
-	    var target = this;
-	    return target.split(search).join(replacement);
+	String.prototype.replaceAll = function (search, replacement) {
+		var target = this;
+		return target.split(search).join(replacement);
 	};
 
 
 
 	function hasClass(obj, c) {
-	  return new RegExp('(\\s|^)' + c + '(\\s|$)').test(obj.className);
+		return new RegExp('(\\s|^)' + c + '(\\s|$)').test(obj.className);
 	}
 
 	function addClass(obj, c) {
-	  if (!hasClass(obj, c)) {
-	    obj.className += ' ' + c;
-	  }
+		if (!hasClass(obj, c)) {
+			obj.className += ' ' + c;
+		}
 	}
 
 	function removeClass(obj, c) {
-	  if (hasClass(obj, c)) {
-	    obj.className = obj.className.replace(new RegExp('(\\s|^)' + c + '(\\s|$)'), ' ').replace(/\s+/g, ' ').replace(/^\s|\s$/, '');
-	  }
+		if (hasClass(obj, c)) {
+			obj.className = obj.className.replace(new RegExp('(\\s|^)' + c + '(\\s|$)'), ' ').replace(/\s+/g, ' ').replace(/^\s|\s$/, '');
+		}
 	}
 
 	function findPos(node) {
-	    var node = node; 	
-	    var curtop = 0;
-	    var curtopscroll = 0;
-	    var curleft = 0;
-	    var curleftscroll = 0;
-	    //var needHTML = true;
-	    if (node.offsetParent) {
-	        do {
-	        	if(node.offsetParent && node.offsetParent == document.getElementsByTagName("html")[0]){
-	        		// needHTML = false;
-	        	}
-	            curtop += node.offsetTop;
-	            curtopscroll += node.offsetParent ? node.offsetParent.scrollTop : 0;
-	            curleft += node.offsetLeft;
-	            curleftscroll += node.offsetParent ? node.offsetParent.scrollLeft : 0;
+		var node = node;
+		var curtop = 0;
+		var curtopscroll = 0;
+		var curleft = 0;
+		var curleftscroll = 0;
+		//var needHTML = true;
+		if (node.offsetParent) {
+			do {
+				if (node.offsetParent && node.offsetParent == document.getElementsByTagName("html")[0]) {
+					// needHTML = false;
+				}
+				curtop += node.offsetTop;
+				curtopscroll += node.offsetParent ? node.offsetParent.scrollTop : 0;
+				curleft += node.offsetLeft;
+				curleftscroll += node.offsetParent ? node.offsetParent.scrollLeft : 0;
 
-	        } while (node = node.offsetParent);
+			} while (node = node.offsetParent);
 
-	        // if(needHTML){
-	        // 	curtopscroll += document.getElementsByTagName("html")[0].scrollTop;
-	        // 	curleftscroll += document.getElementsByTagName("html")[0].scrollLeft;
-	        // }
-	        
+			// if(needHTML){
+			// 	curtopscroll += document.getElementsByTagName("html")[0].scrollTop;
+			// 	curleftscroll += document.getElementsByTagName("html")[0].scrollLeft;
+			// }
 
 
-	        return [curleft - curleftscroll, curtop - curtopscroll];
-	    }
+
+			return [curleft - curleftscroll, curtop - curtopscroll];
+		}
 	}
 
 	var identifier = identifier || [];
@@ -75,7 +75,7 @@ function SplitText(identifier, vars){
 	};
 
 
-	this.HTMLobjects=[];
+	this.HTMLobjects = [];
 	this.vars = {};
 	this.originalHTML = [];
 
@@ -84,35 +84,35 @@ function SplitText(identifier, vars){
 	this.chars = [];
 
 	//if the identifier isn't an array, make it one.  If it already is, don't worry.  :)
-	if(!Array.isArray(identifier)){
+	if (!Array.isArray(identifier)) {
 		identifier = [identifier];
 	}
 
 	//itterate through the array
-	for(var i = 0;i<identifier.length;i++){
-		
-		
+	for (var i = 0; i < identifier.length; i++) {
+
+
 		//if it is an html element, simply add it
-		if(identifier[i].nodeType==1){
+		if (identifier[i].nodeType == 1) {
 			this.HTMLobjects.push(identifier[i]);
 		}
 
 		//if jquery Element add each html Element
 		if (window.jQuery && identifier[i] && (identifier[i] instanceof jQuery || identifier[i].constructor.prototype.jquery)) {
 			//itterate through array of html elements inside jquery object
-			for(var j = 0; j<identifier[i].length;j++){
+			for (var j = 0; j < identifier[i].length; j++) {
 				//Check that it is an html element before appending it
-				if(identifier[i][j].nodeType==1){
+				if (identifier[i][j].nodeType == 1) {
 					this.HTMLobjects.push(identifier[i][j]);
 				}
 			}
 		}
 
 		//if it's a string, try query selector all
-		if(typeof identifier[i] == "string"){
+		if (typeof identifier[i] == "string") {
 			elements = document.querySelectorAll(identifier[i]);
-			for(var j = 0; j<elements.length;j++){
-				if(elements[j].nodeType==1){
+			for (var j = 0; j < elements.length; j++) {
+				if (elements[j].nodeType == 1) {
 					this.HTMLobjects.push(elements[j]);
 				}
 			}
@@ -121,22 +121,22 @@ function SplitText(identifier, vars){
 	}
 
 	//if there is an object of variables replace defaults otherwise use defaults
-	if(vars && typeof vars == "object" && vars !== null){
+	if (vars && typeof vars == "object" && vars !== null) {
 
 		//if type is passed and it's a string, try and validate otherwise use default
-		if(vars.type && typeof vars.type == "string"){
+		if (vars.type && typeof vars.type == "string") {
 			vars.type = vars.type.split(",");
-			var possible = ["chars","words","lines"];
+			var possible = ["chars", "words", "lines"];
 			var use = [];
-			for(var i = 0;i<vars.type.length;i++){
-				if(possible.indexOf(vars.type[i].toLowerCase())!=-1 && use.indexOf(vars.type[i].toLowerCase())==-1){
+			for (var i = 0; i < vars.type.length; i++) {
+				if (possible.indexOf(vars.type[i].toLowerCase()) != -1 && use.indexOf(vars.type[i].toLowerCase()) == -1) {
 					use.push(vars.type[i].toLowerCase());
 				} else {
 					console.error(vars.type[i] + "is not a valid type");
 				}
 			}
 
-			if(use.length==0){
+			if (use.length == 0) {
 				this.vars.type = defaults.type;
 			} else {
 				this.vars.type = use.join(",");
@@ -149,30 +149,30 @@ function SplitText(identifier, vars){
 
 
 		//if charsClass is set then use it
-		this.vars.charsClass = (vars.charsClass && typeof vars.charsClass == "string")?vars.charsClass:defaults.charsClass;
+		this.vars.charsClass = (vars.charsClass && typeof vars.charsClass == "string") ? vars.charsClass : defaults.charsClass;
 
 		//if wordsClass is set then use it
-		this.vars.wordsClass = (vars.wordsClass && typeof vars.wordsClass == "string")?vars.wordsClass:defaults.wordsClass;
+		this.vars.wordsClass = (vars.wordsClass && typeof vars.wordsClass == "string") ? vars.wordsClass : defaults.wordsClass;
 
 		//if linesClass is set then use it
-		this.vars.linesClass = (vars.linesClass && typeof vars.linesClass == "string")?vars.linesClass:defaults.linesClass;
+		this.vars.linesClass = (vars.linesClass && typeof vars.linesClass == "string") ? vars.linesClass : defaults.linesClass;
 
 		//greensock's splittext doesn't allow static or null.  null will not set position and leave it to any css on the page
-		var allowedPositions = ["absolute","relative","static","fixed","inherit","initial",null];
-		this.vars.position = (vars.position && allowedPositions.indexOf(vars.position)!=-1)?vars.position:defaults.position;
-	}else{
+		var allowedPositions = ["absolute", "relative", "static", "fixed", "inherit", "initial", null];
+		this.vars.position = (vars.position && allowedPositions.indexOf(vars.position) != -1) ? vars.position : defaults.position;
+	} else {
 		this.vars = duplicateObject(defaults);
 	}
 
 	//Store the original state so we can revert easily
-	for(var i = 0;i<this.HTMLobjects.length;i++){
-		this.originalHTML[i]=this.HTMLobjects[i].innerHTML;
+	for (var i = 0; i < this.HTMLobjects.length; i++) {
+		this.originalHTML[i] = this.HTMLobjects[i].innerHTML;
 	}
 
 	//add the revert function
-	this.revert = function(){
-		for(var i = 0;i<this.HTMLobjects.length;i++){
-			this.HTMLobjects[i].innerHTML=this.originalHTML[i];
+	this.revert = function () {
+		for (var i = 0; i < this.HTMLobjects.length; i++) {
+			this.HTMLobjects[i].innerHTML = this.originalHTML[i];
 		}
 	}
 
@@ -190,8 +190,8 @@ function SplitText(identifier, vars){
 
 	this.vars.type = this.vars.type.split(",");
 
-	for(var i = 0;i<this.HTMLobjects.length;i++){
-			
+	for (var i = 0; i < this.HTMLobjects.length; i++) {
+
 		var current = this.HTMLobjects[i];
 
 		//remove tags from element
@@ -199,13 +199,13 @@ function SplitText(identifier, vars){
 		current.innerHTML = current.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
 
 		var currentLists = {
-			lines:[],
-			words:[],
-			chars:[]
+			lines: [],
+			words: [],
+			chars: []
 		};
 
 		//Split Lines
-		if(this.vars.type.indexOf("lines")!=-1){
+		if (this.vars.type.indexOf("lines") != -1) {
 			var text = current.innerHTML;
 			var words = text.split(' ');
 			var splitPoints = [];
@@ -213,12 +213,12 @@ function SplitText(identifier, vars){
 			var height = current.offsetHeight;
 
 			//work out where the splits are
-			for(var j = 1; j < words.length; j++){
-			    current.innerHTML = current.innerHTML + ' ' + words[j];
-			    if(current.offsetHeight > height){
-			        height = current.offsetHeight;
-			        splitPoints.push(current.innerHTML.length - (words[j].length+1));
-			    }
+			for (var j = 1; j < words.length; j++) {
+				current.innerHTML = current.innerHTML + ' ' + words[j];
+				if (current.offsetHeight > height) {
+					height = current.offsetHeight;
+					splitPoints.push(current.innerHTML.length - (words[j].length + 1));
+				}
 			}
 			//add the last line
 			splitPoints.push(current.innerHTML.length);
@@ -228,45 +228,45 @@ function SplitText(identifier, vars){
 			current.innerHTML = "";
 
 
-			for(var j = 0; j < splitPoints.length; j++){
-			   	var lineStart = (j==0)?0:splitPoints[j-1]+1;
-			   	var lineEnd = (j==splitPoints.length-1)?text.length:splitPoints[j];
+			for (var j = 0; j < splitPoints.length; j++) {
+				var lineStart = (j == 0) ? 0 : splitPoints[j - 1] + 1;
+				var lineEnd = (j == splitPoints.length - 1) ? text.length : splitPoints[j];
 
 
-			   	var div = document.createElement("div");
+				var div = document.createElement("div");
 
-			   	div.style.display = "block";
-			   	if(this.vars.linesClass!==undefined && this.vars.linesClass!="undefined"){
-			   		this.class = this.vars.linesClass.replace("++",j+1);
-			   	}
-			   	div.innerHTML = text.substring(lineStart,lineEnd)
-			   	current.appendChild(div);
+				div.style.display = "block";
+				if (this.vars.linesClass !== undefined && this.vars.linesClass != "undefined") {
+					this.class = this.vars.linesClass.replace("++", j + 1);
+				}
+				div.innerHTML = text.substring(lineStart, lineEnd)
+				current.appendChild(div);
 
 
-			   	if(this.vars.position !== null){
-			   		if(this.vars.position =="absolute"){
-			   			div.toBe = {
-			   				top: div.offsetTop,
-			   				left: div.offsetLeft
-			   			};
-			   			div.style.position = "relative";
-			   		} else if(this.vars.position =="fixed"){
-			   			var pos = findPos(div);
-			   			div.toBe = {
-			   				top: pos[1],
-			   				left: pos[0]
-			   			}
-			   			div.style.position = "relative";
-			   		} else {
-			   			div.style.position = this.vars.position;
-			   		}
-			   	}
+				if (this.vars.position !== null) {
+					if (this.vars.position == "absolute") {
+						div.toBe = {
+							top: div.offsetTop,
+							left: div.offsetLeft
+						};
+						div.style.position = "relative";
+					} else if (this.vars.position == "fixed") {
+						var pos = findPos(div);
+						div.toBe = {
+							top: pos[1],
+							left: pos[0]
+						}
+						div.style.position = "relative";
+					} else {
+						div.style.position = this.vars.position;
+					}
+				}
 
-			   	currentLists.lines.push(div);
+				currentLists.lines.push(div);
 
 			}
 
-			
+
 		}
 
 
@@ -275,45 +275,45 @@ function SplitText(identifier, vars){
 
 
 		//split the words
-		if(this.vars.type.indexOf("words")!=-1){
-			function splitWords(parent,st){
+		if (this.vars.type.indexOf("words") != -1) {
+			function splitWords(parent, st) {
 				var startTag = "<div style='display:inline-block;'>";
 				var endTag = "</div>";
-				parent.innerHTML = startTag + parent.innerHTML.replaceAll(" ",(endTag+ " " +startTag)) + endTag;
+				parent.innerHTML = startTag + parent.innerHTML.replaceAll(" ", (endTag + " " + startTag)) + endTag;
 
 				var nodes = parent.querySelectorAll("div");
 
-				for(var j = 0; j<nodes.length;j++){
-					if(st.vars.wordsClass!==undefined && st.vars.wordsClass!="undefined"){
-						addClass(nodes[j],st.vars.wordsClass.replaceAll("++",j+1));
+				for (var j = 0; j < nodes.length; j++) {
+					if (st.vars.wordsClass !== undefined && st.vars.wordsClass != "undefined") {
+						addClass(nodes[j], st.vars.wordsClass.replaceAll("++", j + 1));
 					}
 
-					if(st.vars.position !== null){
-				   		if(st.vars.position =="absolute"){
-				   			nodes[j].toBe = {
-				   				top: nodes[j].offsetTop,
-				   				left: nodes[j].offsetLeft
-				   			};
-				   			nodes[j].style.position = "relative";
-				   		} else if(st.vars.position =="fixed"){
-				   			var pos = findPos(nodes[j]);
-				   			nodes[j].toBe = {
-				   				top: pos[1],
-				   				left: pos[0]
-				   			}
-				   			nodes[j].style.position = "relative";
-				   		} else {
-				   			nodes[j].style.position = st.vars.position;
-				   		}
-			   		}
+					if (st.vars.position !== null) {
+						if (st.vars.position == "absolute") {
+							nodes[j].toBe = {
+								top: nodes[j].offsetTop,
+								left: nodes[j].offsetLeft
+							};
+							nodes[j].style.position = "relative";
+						} else if (st.vars.position == "fixed") {
+							var pos = findPos(nodes[j]);
+							nodes[j].toBe = {
+								top: pos[1],
+								left: pos[0]
+							}
+							nodes[j].style.position = "relative";
+						} else {
+							nodes[j].style.position = st.vars.position;
+						}
+					}
 
 					currentLists.words.push(nodes[j]);
 				}
 			}
 
 			//if it has been split by lines, split each line by words
-			if(this.vars.type.indexOf("lines")!=-1){
-				for(var j = 0; j<currentLists.lines.length;j++){
+			if (this.vars.type.indexOf("lines") != -1) {
+				for (var j = 0; j < currentLists.lines.length; j++) {
 					splitWords(currentLists.lines[j], this);
 				}
 			} else {
@@ -324,49 +324,49 @@ function SplitText(identifier, vars){
 
 
 		//split the characters
-		if(this.vars.type.indexOf("chars")!=-1){
-			function splitChars(parent,st){
+		if (this.vars.type.indexOf("chars") != -1) {
+			function splitChars(parent, st) {
 				var startTag = "<div style='display:inline-block;'>";
 				var endTag = "</div>";
 				var specials = (parent.innerHTML.match(/(&\w+;)/g));
-				parent.innerHTML = startTag + parent.innerHTML.replace(/&\w+;/g,"ህ").split("").join(endTag+startTag) + endTag;
+				parent.innerHTML = startTag + parent.innerHTML.replace(/&\w+;/g, "ህ").split("").join(endTag + startTag) + endTag;
 
 				var nodes = parent.querySelectorAll("div");
 
-				for(var j = 0; j<nodes.length;j++){
-					if(st.vars.charsClass!==undefined && st.vars.charsClass!="undefined"){
-						var newClass = st.vars.charsClass.replaceAll("++",j+1);
-						if(j!=nodes.length-1){
-							newClass = newClass.replaceAll("**",nodes[j].innerHTML+nodes[j+1].innerHTML);
+				for (var j = 0; j < nodes.length; j++) {
+					if (st.vars.charsClass !== undefined && st.vars.charsClass != "undefined") {
+						var newClass = st.vars.charsClass.replaceAll("++", j + 1);
+						if (j != nodes.length - 1) {
+							newClass = newClass.replaceAll("**", nodes[j].innerHTML + nodes[j + 1].innerHTML);
 						} else {
-							newClass=newClass.replaceAll("**","");
+							newClass = newClass.replaceAll("**", "");
 						}
-						addClass(nodes[j],newClass);
+						addClass(nodes[j], newClass);
 					}
 
-					if(st.vars.position !== null){
-				   		if(st.vars.position =="absolute"){
-				   			nodes[j].toBe = {
-				   				top: nodes[j].offsetTop,
-				   				left: nodes[j].offsetLeft
-				   			};
-				   			nodes[j].style.position = "relative";
-				   		} else if(st.vars.position =="fixed"){
-				   			var pos = findPos(nodes[j]);
-				   			nodes[j].toBe = {
-				   				top: pos[1],
-				   				left: pos[0]
-				   			}
-				   			nodes[j].style.position = "relative";
-				   		} else {
-				   			nodes[j].style.position = st.vars.position;
-				   		}
-			   		}
+					if (st.vars.position !== null) {
+						if (st.vars.position == "absolute") {
+							nodes[j].toBe = {
+								top: nodes[j].offsetTop,
+								left: nodes[j].offsetLeft
+							};
+							nodes[j].style.position = "relative";
+						} else if (st.vars.position == "fixed") {
+							var pos = findPos(nodes[j]);
+							nodes[j].toBe = {
+								top: pos[1],
+								left: pos[0]
+							}
+							nodes[j].style.position = "relative";
+						} else {
+							nodes[j].style.position = st.vars.position;
+						}
+					}
 
-			   		if(nodes[j].innerHTML=="ህ"){
-			   			nodes[j].innerHTML = specials[0];
-			   			specials.splice(0,1);
-			   		}
+					if (nodes[j].innerHTML == "ህ") {
+						nodes[j].innerHTML = specials[0];
+						specials.splice(0, 1);
+					}
 
 					currentLists.chars.push(nodes[j]);
 				}
@@ -375,12 +375,12 @@ function SplitText(identifier, vars){
 
 			//if it has been split by words, split each word by characters
 			//if it has only be split by lines, split each line by characters
-			if(this.vars.type.indexOf("words")!=-1){
-				for(var j = 0; j<currentLists.words.length;j++){
+			if (this.vars.type.indexOf("words") != -1) {
+				for (var j = 0; j < currentLists.words.length; j++) {
 					splitChars(currentLists.words[j], this);
 				}
-			} else if(this.vars.type.indexOf("lines")!=-1){
-				for(var j = 0; j<currentLists.lines.length;j++){
+			} else if (this.vars.type.indexOf("lines") != -1) {
+				for (var j = 0; j < currentLists.lines.length; j++) {
 					splitChars(currentLists.lines[j], this);
 				}
 			} else {
@@ -391,7 +391,7 @@ function SplitText(identifier, vars){
 		}
 
 
-		if(this.vars.position == "absolute" || this.vars.position == "fixed"){
+		if (this.vars.position == "absolute" || this.vars.position == "fixed") {
 			for (var j = currentLists.chars.length - 1; j >= 0; j--) {
 				currentLists.chars[j].style.width = currentLists.chars[j].offsetWidth + "px";
 				currentLists.chars[j].style.height = currentLists.chars[j].offsetHeight + "px";
@@ -425,7 +425,7 @@ function SplitText(identifier, vars){
 				currentLists.lines[j].style.position = this.vars.position;
 			}
 		}
-		
+
 
 
 		this.lines = this.lines.concat(currentLists.lines);
